bba296eea52eeceb7f28f992077ec01626eb0216
Priya <priyankapriyajp@gmail.com>
2026-05-23 22:56:15 +0530

    fix broken image paths

--- src/sections/Hero/components/HeroTitle.tsx
+++ src/sections/Hero/components/HeroTitle.tsx
@@ -3,12 +3,12 @@ export const HeroTitle = () => {
     <div className="relative items-center box-border caret-transparent flex flex-col justify-center min-h-[auto] min-w-[auto] outline-[3px] gap-y-5 z-[3] md:gap-y-2">
 
       <div
-        aria-label="LANES®"
+        aria-label="REELZ ON GO®"
         className="items-start box-border caret-transparent flex justify-center min-h-[auto] min-w-[auto] opacity-90 outline-[3px] pointer-events-none p-0 md:p-4"
       >
         <div className="box-border caret-transparent min-h-[auto] min-w-[auto] outline-[3px]">
           <h1 className="text-white text-[107.2px] font-black box-border caret-transparent inline-block tracking-[-7.504px] leading-[96.48px] opacity-90 outline-[3px] font-utara md:text-[272px] md:tracking-[-19.04px] md:leading-[244.8px]">
-            {["L", "A", "N", "E", "S"].map((letter, i) => (
+            {["R", "E", "E", "L", "Z"].map((letter, i) => (
               <div
                 key={i}
                 className="relative text-[107.2px] box-border caret-transparent inline-block blur-0 tracking-[-7.504px] leading-[96.48px] outline-[3px] md:text-[272px] md:tracking-[-19.04px] md:leading-[244.8px]"
